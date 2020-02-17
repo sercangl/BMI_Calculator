55be347dee0a432eb56a1a1e390c9cacd4a070cf
sercangl <sercan1540@gmail.com>
2020-02-17 20:19:06 +0300

    some constants organized and some components added

--- bmi_calculator/lib/constants.dart
+++ bmi_calculator/lib/constants.dart
@@ -4,6 +4,11 @@ const labelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8DbE98));
 const kNumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
 const kLargeButtonTextStyle =
     TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
+const kTitleTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold);
+const resultTextStyle = TextStyle(
+    color: Color(0xFF24D876), fontSize: 22.0, fontWeight: FontWeight.bold);
+const kBMITextStyle = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
+const kBodyTextStyle = TextStyle(fontSize: 22);
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
--- bmi_calculator/lib/resultsPage.dart
+++ bmi_calculator/lib/resultsPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'reusable-Card.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({Key key}) : super(key: key);
@@ -7,7 +9,33 @@ class ResultsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Results")),
-      body: Text('Hello'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Text('Your Results', style: kTitleTextStyle),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableWidget(
+              colour: activeCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('OVERWEIGHT', style: resultTextStyle),
+                  Text('18.3', style: kBMITextStyle),
+                  Text('Your BMI results is quite low, you should ear more!',
+                      style: kBodyTextStyle),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
